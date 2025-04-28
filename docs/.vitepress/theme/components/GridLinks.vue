dd40caf48e3b127137e494763ea6a4fec76603cf
Owais Ughratdar <owais.ughratdar@gmail.com>
2025-04-27 21:28:57 -0400

    set up links section component

---
+++ docs/.vitepress/theme/components/GridLinks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="custom-links-grid">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.custom-links-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 8px;
+  margin-top: 16px;
+  width: 100%;
+}
+
+/* Responsive styling */
+@media (max-width: 640px) {
+  .custom-links-grid {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
